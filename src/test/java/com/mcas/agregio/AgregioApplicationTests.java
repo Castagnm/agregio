38a525f4a46add422cfd8ab679ac773d7ce2d3bf
Maxime Castagnet <maxime.castagnet@outlook.fr>
2024-05-30 10:54:34 +0200

    initialisation du projet

---
+++ src/test/java/com/mcas/agregio/AgregioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcas.agregio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
