51161fc27f3467e6ff761da8107be12b488d5bec
Maxime Castagnet <maxime.castagnet@outlook.fr>
2024-05-30 11:24:18 +0200

    Park API get selling parks for specific mark initial implementation

--- src/main/java/com/mcas/agregio/park/ParkController.java
+++ src/main/java/com/mcas/agregio/park/ParkController.java
@@ -1,12 +1,21 @@
 package com.mcas.agregio.park;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mcas.agregio.market.Market;
 
 @RestController
 public class ParkController {
 
     public Park createPark(String name, ProductionType productionType) {
         return new Park(name, productionType);
+    }
+
+    public List<Park> getSellingParks(Market market) {
+        return new ArrayList<>();
     }
     
 }
--- src/test/java/com/mcas/agregio/ParkControllerTests.java
+++ src/test/java/com/mcas/agregio/ParkControllerTests.java
@@ -3,9 +3,12 @@ package com.mcas.agregio;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.mcas.agregio.market.Market;
 import com.mcas.agregio.park.Park;
 import com.mcas.agregio.park.ParkController;
 import com.mcas.agregio.park.ProductionType;
@@ -31,5 +34,15 @@ class ParkControllerTests {
         assertNotNull(park.getId());
         assertEquals(name, park.getName());
         assertEquals(productionType, park.getProductionType());
+    }
+
+    @Test
+    void shouldReturnListOfParksForMarket() {
+        // Given
+        Market market = Market.PRIMARY_RESERVE;
+        // When
+        List<Park> parks = parkController.getSellingParks(market);
+        // Then
+        assertNotNull(parks);
     }
 }
